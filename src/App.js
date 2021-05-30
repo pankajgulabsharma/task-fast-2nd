9386ff5f88214e8c9ad6ae51231fac00b5888b93
pankaj sharma <pankajsharma123@gmail.com>
2021-05-30 22:51:16 +0530

    commiting

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import { Grid, Paper } from "@material-ui/core";
+import Header from "./components/Header/Header";
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: "center",
+    color: theme.palette.text.secondary,
+  },
+  sideImage: {
+    border: "1px solid red",
+    width: "100%",
+    height: "100%",
+  },
+}));
 function App() {
+  const classes = useStyles();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Grid container>
+        <Grid item xs={12} sm={12} md={7} lg={7}>
+          <Header />
+          <Paper className={classes.paper}>xs=12</Paper>
+        </Grid>
+        <Grid item xs={12} sm={12} md={5} lg={5}>
+          <img
+            src={`${process.env.PUBLIC_URL}/assets/images/flowerimage.jpg`}
+            className={classes.sideImage}
+          />
+        </Grid>
+      </Grid>
     </div>
   );
 }
---
+++ src/components/Header/Header.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { Button, IconButton, Toolbar } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles({
+  root: {
+    // flexGrow: 1,
+    // border: "1px solid red",
+    display: "flex",
+    justifyContent: "space-between",
+    cursor: "pointer",
+  },
+  image: {
+    width: "150px",
+    height: "50px",
+    backgroundColor: "#fff",
+  },
+  button: {
+    color: "#000",
+    // border: "1px solid red",
+  },
+});
+function Header() {
+  const classes = useStyles();
+  return (
+    <Toolbar className={classes.root}>
+      <img
+        src={`${process.env.PUBLIC_URL}/assets/images/spark.png`}
+        className={classes.image}
+      />
+      <Button className={classes.button}>Sign up</Button>
+    </Toolbar>
+  );
+}
+
+export default Header;
